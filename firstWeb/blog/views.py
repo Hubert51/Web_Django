f48ad6260291311262a95f71ceda354990518dfc
Hubert51 <gengr@rpi.edu>
2016-11-28 21:17:52 -0500

    Remove wrong file and add useful part

--- firstWeb/blog/views.py
+++ firstWeb/blog/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+from django.http import HttpResponse
+from django.template import RequestContext
+import datetime
 
 # Create your views here.
+
+def home(request):
+	time = datetime.datetime.now()
+
+	return render(request,'index.html',locals() )  
